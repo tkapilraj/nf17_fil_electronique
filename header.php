9f74edb81c658687bfd08fd2725055df6862581e
daniel <daniel-artchounin@users.noreply.github.com>
2015-05-17 15:07:59 +0200

    modifications html5 2

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta charset="utf-8" />
 		<title>Le-fil électronique</title>
 		<meta name="keywords" content="" />
 		<meta name="description" content="" />
