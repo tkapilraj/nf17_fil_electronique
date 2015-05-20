2250537a0fdf9e736f24098a556e8ecbcb11a6d5
inkhey <mail@inkey-art.net>
2015-05-20 23:48:45 +0200

    Protection injection SQL consulter article

--- modeles/m_consulter_article.php
+++ modeles/m_consulter_article.php
@@ -2,6 +2,9 @@
 	// fonction permettant de récupérer la liste des article conçus
 	// par un rédacteur dont le pseudo est transmis en paramètre
 	function getArticlesAuteur($connexion, $pseudo){
+		// on protège les entrées
+		$pseudo = pg_escape_string($pseudo);
+		// requête
 		$requete = "SELECT article
 		FROM red_concoit_art
 		WHERE redacteur ='$pseudo';";
