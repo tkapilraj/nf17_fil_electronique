12613bc29bf655f5c03129d63450ccc4ad406b9c
xuminzhe <minzhe.xu@yahoo.com>
2015-06-16 23:33:46 +0200

    Update m_consulter_article_soumis.php

--- modeles/m_consulter_article_soumis.php
+++ modeles/m_consulter_article_soumis.php
@@ -2,8 +2,11 @@
 //fonction pour permettre l'editeur de consulter les articles soumission
   function getArticleSoumis($connexion,$pseudo){
     $pseudo   = pg_escape_string($pseudo);
-    $requete  = "SELECT article,soumis 
-    FROM art_appartient_soum";
+    $requete  = "SELECT aa.article article, aa.soumis soumis
+    FROM editeur e, Soumission s, art_appartient_soum aa
+    WHERE e.pseudo = '$pseudo' AND e.comite = s.comite_editorial AND s._date = aa.soumis AND aa.article NOT IN (
+    SELECT article FROM changement_etat_art_ed WHERE aa.soumis < _date
+    );";
     $resultat = pg_query($connexion,$requete);
     return $resultat;
   }
